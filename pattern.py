8a7f28bb0a84b923d62eb60750daa6b520e5a999
SAMEER-24680 <sumairasohail523@gmail.com>
2026-09-11 22:32:04 +0500

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,6 @@
+print("HALF PYRAMID PATTERN OF STARS (*) : ")
+a = int(input("ENTER THE NUMBER OF ROWS YOU WANT TO DISPLAY : "))
+for i in range(a):
+    for b in range(i+1):
+        print(" * ", end = "")
+    print( )
